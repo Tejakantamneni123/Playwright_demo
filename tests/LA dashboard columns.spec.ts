458a0fb3860f9c82a3813d13a0e2ceff35d480eb
Tejakantamneni123 <ravikantamneni94@gmail.com>
2025-11-04 16:30:13 +0530

    lefah

--- tests/LA dashboard columns.spec.ts
+++ tests/LA dashboard columns.spec.ts
@@ -14,7 +14,7 @@ await page.getByRole('textbox', { name: 'Enter your email, phone, or' }).fill('L
 await page.getByRole('button', { name: 'Next' }).click(); // next button 
 await page.getByRole('textbox', { name: 'Enter the password for' }).fill('NH@mA4g3$pn?qZF!');// password field
 await page.getByRole('button', { name: 'Sign in' }).click();; // sign-in button
-await page.waitForTimeout(5000);
+await page.waitForLoadState(); // wait for page loaded completely
 // navigate to LA dashboard and verify columns
 await page.getByAltText("Group Engagement dashboard").click(); // LA dashboard
 await page.locator('div').filter({ hasText: /^Issuer Name$/ }).isVisible();
